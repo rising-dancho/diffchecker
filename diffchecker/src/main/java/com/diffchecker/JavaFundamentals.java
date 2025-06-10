7eeac6ac957a51179e68ac833e52ac392f99301a
rising-dancho <zenlaws02@gmail.com>
2025-06-10 20:05:30 +0800

    feat: in progress, learning java again

---
+++ diffchecker/src/main/java/com/diffchecker/JavaFundamentals.java
@@ -0,0 +1,17 @@
+package com.diffchecker;
+
+public class JavaFundamentals {
+  public static void main(String[] args) {
+    // EXAMPLE 1: square of each number
+    // System.out.println(1 + " squared is = " + (1*1));
+    // System.out.println(2 + " squared is = " + (2*2));
+    // System.out.println(3 + " squared is = " + (3*3));
+    // System.out.println(4 + " squared is = " + (4*4));
+    // System.out.println(5 + " squared is = " + (5*5));
+
+    for (int i = 1; i <= 100; i++) {
+      // System.out.println("i = " + i);
+      System.out.println(i + " squared is = " + (i * i));
+    }
+  }
+}
