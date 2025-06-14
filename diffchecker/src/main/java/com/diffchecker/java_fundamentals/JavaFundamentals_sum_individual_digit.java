5fe7384a1533f6c639f8384fff980e2c2e1b7665
rising-dancho <zenlaws02@gmail.com>
2025-06-15 04:14:12 +0800

    feat: in progress, learned how to extract each digit programmatically of a given digit and summate it

---
+++ diffchecker/src/main/java/com/diffchecker/java_fundamentals/JavaFundamentals_sum_individual_digit.java
@@ -0,0 +1,33 @@
+package com.diffchecker.java_fundamentals;
+
+import java.util.Scanner;
+
+public class JavaFundamentals_sum_individual_digit {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a number and i will sum each of it's digits: ");
+    int number = scanner.nextInt();
+
+    int original = number;
+
+    int sum = 0;
+    int last_digit = 0;
+
+    while (number != 0) {
+      // THIS EXTRACTS THE LAST DIGIT OF THE ENTERED NUMBER:
+      // eg. 56 --> last digit is 6.. (loop iterates) next digit --> 5
+
+      last_digit = number % 10;
+      System.out.println(last_digit);
+
+      sum = sum + last_digit;
+      number = number / 10;
+      // System.out.println(number);
+    }
+
+    System.out.println();
+    System.out.println("Sum of each digit of the entered number " + original + " is : " + sum);
+    scanner.close();
+  }
+}
