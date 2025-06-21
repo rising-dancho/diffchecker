857b2d81db0e33ee7f7b66d02ac8f38383384ae0
rising-dancho <zenlaws02@gmail.com>
2025-06-21 18:54:38 +0800

    feat: breaking down how to get prime programatically

--- diffchecker/src/main/java/com/diffchecker/backup/getting_prime.java
+++ diffchecker/src/main/java/com/diffchecker/backup/getting_prime.java
@@ -44,4 +44,41 @@ public class getting_prime {
                 System.out.print("Prime numbers from 1 to n are: " + primeNumbers);
 
         }
+
+  // Tracing The Inner And Outer Loop
+  /*
+   * n = 2 (CONSTANT VALUE)
+   * 
+   * Step                     Code                Description
+   * Initialization int       i=1                 Variable i is created and initialized to 1 (step 1)
+   * 
+   * Outer Loop Test #1       i=1                 True, we can enter the loop (step 2)
+   * i<=2
+   * 
+   * Body                     {...}               Execute the Inner Loop because i = (1) less than or equal to 2 is
+   * true
+   * 
+   * Inner Loop Test #1       num = 1,            num = i => 1 >= 1 True; execute the code inside the inner loop
+   *                          num >= 1
+   * 
+   * Update Inner Loop        num--               Decrement num, which becomes 0;
+   * 
+   * Update Outer Loop        i++                 Increment i, which becomes 2
+   * 
+   * Outer Loop Test #2       i=2                 True, as long as still true we enter the loop (step 3)
+   * i<=2
+   * 
+   * Body                     {...}               Execute the Inner Loop because i = (2) equal to 2 is true
+   * 
+   * Inner Loop Test #2       num = 2,            num = i => 2 >= 1 True; execute the code inside the inner loop
+   *                          num >= 1
+   * 
+   * Update Inner Loop        num--               Decrement num, which becomes 1;
+   * Update Outer Loop        i++                 Increment i, which becomes 3
+   * 
+   * Outer Loop Test #3       i=3                 False, we do not enter the loop (step 4)
+   *                          i<=2
+   * 
+   * num => 1,2,1
+   */
 }
