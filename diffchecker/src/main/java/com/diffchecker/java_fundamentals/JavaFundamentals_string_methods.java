9e0fe589f1676c265af7287951f60bc2e3266e31
rising-dancho <zenlaws02@gmail.com>
2025-06-23 17:44:22 +0800

    feat: learning java String methods

--- diffchecker/src/main/java/com/diffchecker/java_fundamentals/JavaFundamentals_string_methods.java
+++ diffchecker/src/main/java/com/diffchecker/java_fundamentals/JavaFundamentals_string_methods.java
@@ -6,6 +6,9 @@ public class JavaFundamentals_string_methods {
     // We can create "objects" of Strings. eg. String l = "ligma";
     // We can then use then use its methods ligma.concat("ballz");
 
+    //----> STRING METHODS <----
+
+    // CONCAT
     String l = "ligma";
     System.out.println(l.concat("ballz"));
     // output: ligmaballz
@@ -16,23 +19,34 @@ public class JavaFundamentals_string_methods {
     // Scout scout1 = new Scout("Erwin");
     // Scout scout2 = new Scout("Levi");
 
-    // String methods
+    // CONTAINS
     String assCheeks = "assCheeks";
     System.out.println(assCheeks.contains("ass"));
     // output: true
 
-    // Index of
+    // INDEX OF
     // in java checking indexes always starts with 0
     // eg. "Hello" => (indexes) 0 1 2 3 4
     String abc = "Hello";
     System.out.println(abc.indexOf("o"));
     // output: 4
 
-    // Replace
+    // CHAR AT
+    String cba = "Werld";
+    System.out.println(cba.charAt(2));
+    // output: r
+
+    // REPLACE
     // example.replace(TARGET,REPLACEMENT);
     String example = "Hello World";
     System.out.println(example.replace("Wor", "Bo"));
     // output: Hello Bold
 
+    // TO UPPER CASE, TO LOWER CASE
+    String akiraNakai = "rauh welt";
+    System.out.println(akiraNakai.toUpperCase());
+    // output: RAUH WELT
+    System.out.println(akiraNakai.toLowerCase());
+    // output: rauh welt
   }
 }
