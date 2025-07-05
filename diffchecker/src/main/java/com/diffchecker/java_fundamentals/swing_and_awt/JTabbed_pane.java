44e8769947e7af0687fa27c7035be7c98acbf4d5
rising-dancho <zenlaws02@gmail.com>
2025-07-06 05:19:49 +0800

    feat: added close button to tabs

--- diffchecker/src/main/java/com/diffchecker/java_fundamentals/swing_and_awt/JTabbed_pane.java
+++ diffchecker/src/main/java/com/diffchecker/java_fundamentals/swing_and_awt/JTabbed_pane.java
@@ -1,6 +1,9 @@
 package com.diffchecker.java_fundamentals.swing_and_awt;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.*;
 
 public class JTabbed_pane extends JFrame {
@@ -16,6 +19,32 @@ public class JTabbed_pane extends JFrame {
 
     JTabbedPane tabbedPane = new JTabbedPane();
     tabbedPane.setFocusable(false);
+
+    // Popup menu Delete Tab
+    tabbedPane.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mousePressed(MouseEvent e) {
+        maybeShowPopup(e);
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) {
+        maybeShowPopup(e);
+      }
+
+      private void maybeShowPopup(MouseEvent e) {
+        if (e.isPopupTrigger()) {
+          int index = tabbedPane.indexAtLocation(e.getX(), e.getY());
+          if (index != -1) {
+            JPopupMenu popup = new JPopupMenu();
+            JMenuItem delete = new JMenuItem("Delete Tab");
+            delete.addActionListener(ev -> tabbedPane.remove(index));
+            popup.add(delete);
+            popup.show(e.getComponent(), e.getX(), e.getY());
+          }
+        }
+      }
+    });
 
     JButton addButton = new JButton("+");
     addButton.setBorder(null);
@@ -46,18 +75,39 @@ public class JTabbed_pane extends JFrame {
     tabPanel.setOpaque(false);
 
     JLabel titleLabel = new JLabel(title);
-    // close button
-    ImageIcon originalIcon = new ImageIcon(getClass().getResource("/diffchecker/images/close_def.png"));
-    Image scaledImg = originalIcon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
 
-    JButton closeButton = new JButton(new ImageIcon(scaledImg));
+    // Default icon
+    ImageIcon iconDefault = new ImageIcon(getClass().getResource("/diffchecker/images/close_def.png"));
+    Image scaledDefault = iconDefault.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
+    ImageIcon defaultIcon = new ImageIcon(scaledDefault);
+
+    // Hover icon
+    ImageIcon iconHover = new ImageIcon(getClass().getResource("/diffchecker/images/close_hover.png"));
+    Image scaledHover = iconHover.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
+    ImageIcon hoverIcon = new ImageIcon(scaledHover);
+
+    // Button with default icon
+    JButton closeButton = new JButton(defaultIcon);
     closeButton.setBorder(BorderFactory.createEmptyBorder());
     closeButton.setFocusPainted(false);
     closeButton.setMargin(new Insets(0, 5, 0, 5));
     closeButton.setFocusable(false);
     closeButton.setContentAreaFilled(false);
-    closeButton.setFont(closeButton.getFont().deriveFont(Font.BOLD));
 
+    // Hover effect
+    closeButton.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseEntered(MouseEvent e) {
+        closeButton.setIcon(hoverIcon);
+      }
+
+      @Override
+      public void mouseExited(MouseEvent e) {
+        closeButton.setIcon(defaultIcon);
+      }
+    });
+
+    // Close tab on click
     closeButton.addActionListener(e -> {
       int index = tabbedPane.indexOfTabComponent(tabPanel);
       if (index != -1) {
@@ -69,4 +119,5 @@ public class JTabbed_pane extends JFrame {
     tabPanel.add(closeButton);
     return tabPanel;
   }
+
 }
