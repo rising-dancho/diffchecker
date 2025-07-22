17f6e0703b6c9e6a294f996451f21b241d538814
rising-dancho <zenlaws02@gmail.com>
2025-07-22 19:08:16 +0800

    init: initial commit for the project

---
+++ diffchecker/src/main/java/com/diffchecker/java_fundamentals/swing_and_awt/pos_system/MainScreen.java
@@ -0,0 +1,16 @@
+package com.diffchecker.java_fundamentals.swing_and_awt.pos_system;
+
+import javax.swing.JFrame;
+import java.awt.HeadlessException;
+
+public class MainScreen extends JFrame {
+
+  public MainScreen() throws HeadlessException {
+    createNavButtonsPanel();
+
+  }
+
+  public void createNavButtonsPanel(){
+    
+  }
+}
