4519b91917f4b4b62daf27bca65582091e4c734c
rising-dancho <zenlaws02@gmail.com>
2025-07-25 14:27:04 +0800

    feat: updated the color of the line numbers

--- diffchecker/src/main/java/com/diffchecker/components/LineNumberingTextArea.java
+++ diffchecker/src/main/java/com/diffchecker/components/LineNumberingTextArea.java
@@ -7,13 +7,17 @@ import java.awt.*;
 
 public class LineNumberingTextArea extends JTextArea implements DocumentListener {
     private final JTextArea textArea;
+    private static final Color NUMBER_COLOR = new Color(0x666667); // darker red
+    private static final Color BACKGROUND__COLOR = new Color(0xE2E2E6); // darker red
 
     public LineNumberingTextArea(JTextArea textArea) {
         this.textArea = textArea;
         textArea.getDocument().addDocumentListener(this);
         setEditable(false);
-        setBackground(Color.LIGHT_GRAY);
-        setFont(textArea.getFont());
+        setBackground(BACKGROUND__COLOR);
+        setForeground(NUMBER_COLOR);
+        setFont(textArea.getFont().deriveFont(Font.BOLD));
+        setMargin(new Insets(0, 3, 0, 3)); // Add 5px right padding
         updateLineNumbers();
     }
 
