eda7441686f86e5b559c5e719c294f3624fb1900
rising-dancho <zenlaws02@gmail.com>
2025-07-27 04:37:02 +0800

    feat: currently customizing the tabs

--- diffchecker/src/main/java/com/diffchecker/components/RoundedTabbedPaneUI.java
+++ diffchecker/src/main/java/com/diffchecker/components/RoundedTabbedPaneUI.java
@@ -13,7 +13,8 @@ public class RoundedTabbedPaneUI extends BasicTabbedPaneUI {
   @Override
   protected void installDefaults() {
     super.installDefaults();
-    tabAreaInsets = new Insets(5, 5, 5, 5); // top, left, bottom, right
+    tabInsets = new Insets(6, 20, 6, 20); // top, left, bottom, right (more padding)
+    tabAreaInsets = new Insets(5, 5, 5, 5); // space around tab area
     contentBorderInsets = new Insets(0, 0, 0, 0);
   }
 
@@ -43,7 +44,18 @@ public class RoundedTabbedPaneUI extends BasicTabbedPaneUI {
   @Override
   protected void paintTabBorder(Graphics g, int tabPlacement,
       int tabIndex, int x, int y, int w, int h, boolean isSelected) {
-    // Optional: no border
+    if (!isSelected)
+      return;
+
+    Graphics2D g2 = (Graphics2D) g.create();
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+    g2.setColor(new Color(0x5A5A5A)); // border color
+    int arc = 6;
+    g2.setStroke(new BasicStroke(1.5f));
+    g2.drawRoundRect(x + 2, y + 5, w - 4, h - 10, arc, arc);
+
+    g2.dispose();
   }
 
   @Override
